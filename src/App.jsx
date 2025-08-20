e277d4b360c0c0089c467e026f87bfbe7a4f558f
SCChintha <saicharan.chintha2889@gmail.com>
2025-08-20 07:44:51 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { index: 0, SlideCount: 3 };
+    this.autoSlide = this.autoSlide.bind(this);
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(this.autoSlide, 3000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval); // Clean up the interval on unmount
+  }
+
+  autoSlide() {
+    const nextIndex = (this.state.index + 1) % this.state.SlideCount;
+    const slideRef = document.getElementById("slideRef");
+
+    if (slideRef) {
+      slideRef.style.transform = `translateX(-${nextIndex * 100}%)`;
+    }
+
+    this.setState({ index: nextIndex });
+  }
+
+  render() {
+    const { index } = this.state;
+    return (
+      <>
+        <header>
+          <div className='logo'>Sliding Page - Slide {index}</div>
+        </header>
+        <section>
+          <div className='slider'>
+            <div className='slides' id='slideRef'>
+              <div className='slide s1'></div>
+              <div className='slide s2'></div>
+              <div className='slide s3'></div>
+            </div>
+          </div>
+        </section>
+        <footer>Copyright © 2025. All rights reserved.</footer>
+      </>
+    );
+  }
+}
+
+export default App;
